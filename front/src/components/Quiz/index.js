e2e5860a4e42cc02f818b822bdbe2bdcbfc2160f
Alexandre Richard <alexandrichard99@gmail.com>
2021-02-26 16:54:54 +0100

    Display quizzes ok, starting of the process of one quiz

--- front/src/components/Quiz/index.js
+++ front/src/components/Quiz/index.js
@@ -1,30 +1,21 @@
 // == Import npm
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 // == Import
+import quizzes from 'src/fakeData/Quizzes';
 import './style.scss';
 
 // == Composant
-const Quiz = ({
-  id, category, title, questionNumber,
-}) => (
-  <div className="quiz_exposition">
-    <p>ID du quiz : {id}</p>
-    <p>Category du quiz : {category}</p>
-    <p>Title du quiz : {title}</p>
-    <p>Nombre de questions du quiz : {questionNumber}</p>
-    <Link to={`/quiz/${id}`}>LIEN DU QUIZ IN NON-FOF</Link>
-  </div>
+const Quiz = () => (
+  <>
+    {/* début de ton quiz : créer ta première (première question)
+  créer un 2ème composant (la réponse)
+  suivant => transition, loading mis en true s
+  tra,sition finie, loading se met en false
+  là, on supprime l'ancien et on charge le suivant */}
+    <p>Blabla, voici notre premier quiz</p>
+  </>
 );
-
-Quiz.propTypes = {
-  id: PropTypes.number.isRequired,
-  category: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  questionNumber: PropTypes.number.isRequired,
-};
 
 // == Export
 export default Quiz;
