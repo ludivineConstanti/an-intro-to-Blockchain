04bb71b8bddd2a79cb366f66b502d8a4ab15505e
wahoostik <anthonypaccaud@wanadoo.fr>
2021-02-26 15:09:34 +0100

    make it better

--- back/app/router.js
+++ back/app/router.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const quizController = require('./controllers/quizController');
+const articleController = require('./controllers/articleController');
 
 const router = Router();
 
@@ -11,5 +12,9 @@ router.get('/');
 router.get('/quizzes', quizController.allQuiz);
 router.get('/quiz/:id', quizController.quizById);
 router.get('/quiz/category/:id', quizController.quizByCategoryId);
+
+// Pages article
+router.get('/articles', articleController.allArticles);
+router.get('/articles/:id', articleController.findOneArticle);
 
 module.exports = router;
