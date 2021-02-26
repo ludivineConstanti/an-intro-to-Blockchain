fea8f70ba91c9dee3cc0c0f9f8e1c24a54ff5265
Alexandre Richard <alexandrichard99@gmail.com>
2021-02-26 14:21:00 +0100

    Creation of main components

--- front/src/components/App/index.js
+++ front/src/components/App/index.js
@@ -1,14 +1,50 @@
 // == Import npm
 import React from 'react';
+import {
+  Route,
+  Switch,
+} from 'react-router-dom';
 
 // == Import
 import './style.scss';
+import Header from 'src/components/Header';
 import Home from 'src/components/Home';
+import Quizzes from 'src/components/Quizzes';
+import Login from 'src/components/Login';
+import Register from 'src/components/Register';
+import Error from 'src/components/Error';
+import Profile from 'src/components/Profile';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <Home />
+    <Header />
+    <Switch>
+      {/* Page d'accueil */}
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      {/* Page répertoriant tout les quiz */}
+      <Route path="/quizzes" exact>
+        <Quizzes />
+      </Route>
+      {/* Page de connexion */}
+      <Route path="/login" exact>
+        <Login />
+      </Route>
+      {/* Page d'inscription */}
+      <Route path="/register" exact>
+        <Register />
+      </Route>
+      <Route path="/profile" exact>
+        <Profile />
+      </Route>
+      {/* Page d'erreur */}
+      <Route>
+        <Error />
+      </Route>
+
+    </Switch>
   </div>
 );
 
---
+++ front/src/components/Error/index.js
@@ -0,0 +1,13 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './style.scss';
+
+// == Composant
+const Error = () => (
+  <h1>Retourne pleurer</h1>
+);
+
+// == Export
+export default Error;
