e9f8613feb4746cf2dbd14cf6137c2dcad846670
wahoostik <anthonypaccaud@wanadoo.fr>
2021-02-28 14:16:53 +0100

    test

--- back/app/database.js
+++ back/app/database.js
@@ -6,6 +6,11 @@ const { Pool } = require('pg');
 // PGPASSWORD pour le mot de passe
 // PGDATABASE pour la base de données
 
+// db est un pool de connecteurs de base de données
+// avec Heroku, mon Pool va devoir se connecter à DATABASE_URL
+// par contre, en local, il faut qu'il continue de se connecter avec les variables d'environnement de libpq (PGUSER, PGPASSWORD etc.)
+// mais en fait, Pool se débrouille tout seul comme un grand
+
 const db = new Pool({
     connectionString: process.env.DATABASE_URL
 });
