90024524ed2afad24084285220ea48d98ac636c8
Alexandre Richard <alexandrichard99@gmail.com>
2021-03-01 16:15:59 +0100

    Filling of Database done

--- back/migrations/deploy/init.sql
+++ back/migrations/deploy/init.sql
@@ -1,22 +1,57 @@
 BEGIN;
 
-
-DROP TABLE IF EXISTS "article",
-"user",
-"quiz",
-"user_has_quiz",
+DROP TABLE IF EXISTS "user",
 "category",
+"quiz",
+"user_made_quiz",
 "answer",
 "question",
-"user_has_article";
+"user_read_article";
 
 -- -----------------------------------------------------
--- Table "article"
+-- Table "category"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "article" (
+CREATE TABLE IF NOT EXISTS "category" (
   "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "title" text NOT NULL,
-  "link" text NOT NULL
+  "fr_name" text NOT NULL,
+  "en_name" text NOT NULL
+);
+
+-- -----------------------------------------------------
+-- Table "quiz"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS "quiz" (
+  "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "fr_title" text NOT NULL,
+  "en_title" text NOT NULL,
+  "category_id" int NOT NULL
+);
+
+-- -----------------------------------------------------
+-- Table "question"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS "question" (
+  "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "fr_statement" text NOT NULL,
+  "fr_article_link" text NOT NULL,
+  "fr_article_title" text NOT NULL,
+  "fr_justification" text NOT NULL,
+  "en_statement" text NOT NULL,
+  "en_article_link" text NOT NULL,
+  "en_article_title" text NOT NULL,
+  "en_justification" text NOT NULL,
+  "quiz_id" int NOT NULL REFERENCES "quiz" ("id")
+);
+
+-- -----------------------------------------------------
+-- Table "answer"
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS "answer" (
+  "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "fr_name" text NOT NULL,
+  "en_name" text NOT NULL,
+  "goodAnswer" boolean NOT NULL,
+  "question_id" int NOT NULL REFERENCES "question" ("id")
 );
 
 -- -----------------------------------------------------
@@ -31,65 +66,26 @@ CREATE TABLE IF NOT EXISTS "user" (
 );
 
 -- -----------------------------------------------------
--- Table "quiz"
+-- Table "user_made_quiz"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "quiz" (
-  "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "title" text NOT NULL,
-  "category_id" int NOT NULL
-);
-
--- -----------------------------------------------------
--- Table "utilisateur_has_quiz"
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "user_has_quiz" (
+CREATE TABLE IF NOT EXISTS "user_made_quiz" (
   "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   "scorequiz" int,
-  "doOrNot" boolean NOT NULL,
+  "done" boolean NOT NULL,
   "user_id" int NOT NULL REFERENCES "user"("id"),
   "quiz_id" int NOT NULL REFERENCES "quiz"("id")
 );
 
 -- -----------------------------------------------------
--- Table "categorie"
+-- Table "user_read_article"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "category" (
+CREATE TABLE IF NOT EXISTS "user_read_article" (
   "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "name" text NOT NULL
-);
-
--- -----------------------------------------------------
--- Table "question"
-CREATE TABLE IF NOT EXISTS "question" (
-  "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "thequestion" text NOT NULL,
-  "linkarticle" text NOT NULL,
-  "quiz_id" int NOT NULL REFERENCES "quiz" ("id"),
-  "article_id" int NOT NULL REFERENCES "article" ("id")
-);
-
--- -----------------------------------------------------
--- Table "reponse"
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "answer" (
-  "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "name" text NOT NULL,
-  "rightOrWrong" boolean NOT NULL,
-  "question_id" int NOT NULL REFERENCES "question" ("id")
-);
-
--- -----------------------------------------------------
--- Table "utilisateur_has_article"
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "user_has_article" (
-  "id" int GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  "readOrNot" boolean NOT NULL,
+  "read" boolean NOT NULL,
   "user_id" int NOT NULL REFERENCES "user"("id"),
-  "article_id" int NOT NULL REFERENCES "article"("id")
+  "question_id" int NOT NULL REFERENCES "question"("id")
 );
 
 ALTER TABLE "quiz"
-  ADD FOREIGN KEY ("category_id") REFERENCES "category" ("id");  
-
-
+  ADD FOREIGN KEY ("category_id") REFERENCES "category" ("id");
 COMMIT;
