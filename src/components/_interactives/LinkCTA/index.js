e1217be39e2c8e6f3022720f5ca8a273fffd7c93
ludivineConstanti <ludivine.constanti@outlook.fr>
2021-03-05 17:41:45 +0100

    did more animations for the LinkCTA (quiz icons) on hover

--- src/components/_interactives/LinkCTA/index.js
+++ src/components/_interactives/LinkCTA/index.js
@@ -12,14 +12,14 @@ import Icon from 'src/components/_statics/Icon';
 const LinkCTA = ({ oneQuiz }) => (
   <Link to={`/quiz/${oneQuiz.id}`}>
     <div className="linkCTA">
-      <div className="linkCTA__icon">
+      <div className="linkCTA__iconDiv">
         <Icon category={oneQuiz.categoryId} quiz={oneQuiz.id} />
-        <svg className="linkCTA__icon__frame" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg">
+        <svg className="linkCTA__iconDiv__frame" viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg">
           <path d="M0 10.5V5.09619L4.59619 0.5H10V4.5H4V10.5H0Z" fill="white" />
           <path d="M80 70.5L80 75.9038L75.4038 80.5L70 80.5L70 76.5L76 76.5L76 70.5L80 70.5Z" fill="white" />
           <path d="M10 80.5L4.59619 80.5L-2.00906e-07 75.9038L-4.37114e-07 70.5L4 70.5L4 76.5L10 76.5L10 80.5Z" fill="white" />
           <path d="M70 0.500001L75.4038 0.500001L80 5.09619L80 10.5L76 10.5L76 4.5L70 4.5L70 0.500001Z" fill="white" />
-          <path className="linkCTA__icon__borders" fillRule="evenodd" clipRule="evenodd" d="M5 0.500003V2.5L75 2.5V0.5L5 0.500003ZM2 5.5H0V75.5H2V5.5ZM5 78.5V80.5H75V78.5H5ZM78 5.5H80V75.5H78V5.5Z" fill="white" />
+          <path className="linkCTA__iconDiv__borders" fillRule="evenodd" clipRule="evenodd" d="M5 0.500003V2.5L75 2.5V0.5L5 0.500003ZM2 5.5H0V75.5H2V5.5ZM5 78.5V80.5H75V78.5H5ZM78 5.5H80V75.5H78V5.5Z" fill="white" />
         </svg>
       </div>
       <div className="linkCTA__label">
