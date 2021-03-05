80baa82f7a47446c2fe1e2800abbe3a494146d60
ludivineConstanti <ludivine.constanti@outlook.fr>
2021-03-05 13:01:54 +0100

    worked more on hover effects, still on WIP

--- src/components/_interactives/LinkCTA/index.js
+++ src/components/_interactives/LinkCTA/index.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 // == Import
 import './style.scss';
+import './animation.scss';
 import Icon from 'src/components/_statics/Icon';
 
 // == Composant
@@ -20,7 +21,7 @@ const LinkCTA = ({ category, quiz }) => (
           <path d="M10 80.5L4.59619 80.5L-2.00906e-07 75.9038L-4.37114e-07 70.5L4 70.5L4 76.5L10 76.5L10 80.5Z" fill="white" />
           <path d="M70 0.500001L75.4038 0.500001L80 5.09619L80 10.5L76 10.5L76 4.5L70 4.5L70 0.500001Z" fill="white" />
 
-          <path opacity="0.25" fillRule="evenodd" clipRule="evenodd" d="M5 0.500003V2.5L75 2.5V0.5L5 0.500003ZM2 5.5H0V75.5H2V5.5ZM5 78.5V80.5H75V78.5H5ZM78 5.5H80V75.5H78V5.5Z" fill="white" />
+          <path className="linkCTA__borders" fillRule="evenodd" clipRule="evenodd" d="M5 0.500003V2.5L75 2.5V0.5L5 0.500003ZM2 5.5H0V75.5H2V5.5ZM5 78.5V80.5H75V78.5H5ZM78 5.5H80V75.5H78V5.5Z" fill="white" />
         </svg>
         <div className="linkCTA__label">
           <p>What is it long long long super super super long title lorem ipsum dolor sit amet?</p>
--- src/components/_interactives/_buttons/SubmitButton.js
+++ src/components/_interactives/_buttons/SubmitButton.js
@@ -10,14 +10,14 @@ const SubmitButton = ({ label, className }) => {
   const classDiv = `submitButton submitButton--${className}`;
   const classTriangle = `submitButton__triangle submitButton__triangle--${className}`;
   return (
-    <div className={classDiv}>
-      <button type="submit">{label}</button>
+    <button type="submit" className={classDiv}>
+      {label}
       <div className={classTriangle}>
         <svg viewBox="0 0 12 15" xmlns="http://www.w3.org/2000/svg">
           <path d="M0.800002 1.95744L10.4 7.5L0.800002 13.0426L0.800002 1.95744Z" stroke="white" />
         </svg>
       </div>
-    </div>
+    </button>
   );
 };
 
