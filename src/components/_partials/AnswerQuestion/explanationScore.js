2eabe13341dd80688671d8894e71e08a820cc72e
ludivineConstanti <ludivine.constanti@outlook.fr>
2021-03-13 22:45:05 +0100

    minor changes

--- src/components/_partials/AnswerQuestion/explanationScore.js
+++ src/components/_partials/AnswerQuestion/explanationScore.js
@@ -32,49 +32,55 @@ export default (questionNumber, questionData, userAnswer, language) => {
       }
     }
   }
-  if (userTotalBadAnswers === 0) {
-    if (totalGoodAnswers === userTotalGoodAnswers) {
-      label = switchL ? 'Parfait !' : 'Perfect!';
-      subtitle = switchL ? 'Ces réponses sont correctes' : 'Those answers are correct';
-      if (totalGoodAnswers === 1) {
-        subtitle = switchL ? 'Cette réponse est correcte' : 'Thats the correct answer';
-      }
-    }
-    else if (userTotalGoodAnswers === 1 && userTotalGoodAnswers < totalGoodAnswers) {
-      label = switchL ? 'Dommage' : 'Too bad';
-      subtitle = switchL ? 'Essayez de cliquer plus de case, la prochaine fois' : 'Try clicking more cases next time';
-    }
-    else if (userTotalGoodAnswers === totalGoodAnswers - 1) {
-      subtitle = switchL ? 'Une seule réponse manquante' : 'only one answer missing';
-      if (totalGoodAnswers === 2) {
-        label = switchL ? 'À moitié correct' : 'Half way there';
-      }
-      else if (totalGoodAnswers === 4) {
-        label = switchL ? 'Presque!' : 'Almost!';
-      }
-    }
-    else if (userTotalGoodAnswers === 0) {
-      label = switchL ? 'Non' : 'Nope!';
-      if (userTotalBadAnswers === 0) {
-        subtitle = switchL ? 'Essaie de cliquer sur une case la prochaine fois' : 'Try clicking somewhere next time';
-      }
+
+  if (totalGoodAnswers === userTotalGoodAnswers) {
+    label = switchL ? 'Parfait !' : 'Perfect!';
+    subtitle = switchL ? 'Ces réponses sont correctes' : 'Those answers are correct';
+    if (totalGoodAnswers === 1) {
+      subtitle = switchL ? 'Cette réponse est correcte' : 'That\'s the correct answer';
     }
   }
-  else if (userTotalBadAnswers === totalBadAnswers) {
+
+  if (userTotalGoodAnswers === 1 && userTotalGoodAnswers < totalGoodAnswers) {
+    label = switchL ? 'Dommage' : 'Too bad';
+    subtitle = switchL ? 'Essayez de cliquer plus de case, la prochaine fois' : 'Try clicking more cases next time';
+  }
+
+  if (userTotalGoodAnswers === totalGoodAnswers - 1) {
+    subtitle = switchL ? 'Une seule réponse manquante' : 'only one answer missing';
+    if (totalGoodAnswers === 2) {
+      label = switchL ? 'À moitié correct' : 'Half way there';
+    }
+    if (totalGoodAnswers > 2) {
+      label = switchL ? 'Presque!' : 'Almost!';
+    }
+  }
+
+  if (userTotalGoodAnswers === 0) {
+    label = switchL ? 'Non' : 'Nope!';
+    if (userTotalBadAnswers === 0) {
+      subtitle = switchL ? 'Essaie de cliquer sur une case la prochaine fois' : 'Try clicking somewhere next time';
+    }
+  }
+
+  if (userTotalBadAnswers === totalBadAnswers) {
     if (userTotalGoodAnswers === 0) {
-      label = switchL ? 'Félicitations !' : 'Congratulation!';
+      label = switchL ? 'Bravo !' : 'Congratulation!';
       subtitle = switchL ? 'Vous avez trouvé toute les mauvaises réponses ! La prochaine fois on essaiera les bonnes' : 'You found all the wrong answers! Try finding the right ones next time';
     }
   }
-  else if (userTotalBadAnswers === 3) {
-    label = switchL ? 'Impressionnant !' : 'Amazing!';
+
+  if (userTotalBadAnswers === 3) {
+    label = switchL ? 'Wow !' : 'Amazing!';
     subtitle = switchL ? 'Vous avez cliqué toutes les réponses, sauf la bonne, félicitation !' : 'You clicked all the bad answers, but not the right one, that\'s impressive!';
   }
-  else if (userTotalBadAnswers === 2) {
+
+  if (userTotalBadAnswers === 2) {
     label = 'Hhhmmm';
     subtitle = switchL ? '2 mauvaises réponses... vous les choisissez au hasard?' : '2 wrong answers... are you choosing them randomly?';
   }
-  else if (userTotalBadAnswers === 1) {
+
+  if (userTotalBadAnswers === 1) {
     label = switchL ? 'Faux !' : 'Wrong!';
     subtitle = switchL ? 'Dommage, vous avez 1 mauvaise réponse' : 'Too bad, you\'ve got 1 wrong answer';
     if (userTotalGoodAnswers === 0) {
--- src/components/_partials/AnswerQuestion/index.js
+++ src/components/_partials/AnswerQuestion/index.js
@@ -24,38 +24,8 @@ const AnswerQuestion = ({
   const nextQuestion = () => {
     increaseQuestion(questionNumber + 1);
   };
-  /* const { totalAnswer } = questionData[`question${questionNumber}`];
-  const { id } = questionData[`question${questionNumber}`];
-  const totalGoodAnswers = 0;
-  const goodAnswers = 0;
-  const totalBadAnswers = 0;
-  const badAnswers = 0;
-  const score = 4; */
-  /* for (let i = 1; i < totalAnswer + 1; i += 1) {
-    const currentQuizResponse = questionData[`question${questionNumber}`][`answer${i}`].goodAnswer;
-    const currentUserResponse = userAnswer[`question${id}`][`answer${questionData[`question${questionNumber}`][`answer${i}`].id}`];
-     if (currentQuizResponse) {
-      totalGoodAnswers += 1;
-      if (currentQuizResponse === currentUserResponse) {
-        goodAnswers += 1;
-      }
-      else score -= 1;
-    }
-    else if (!currentQuizResponse) {
-      totalBadAnswers += 1;
-      if (currentQuizResponse !== currentUserResponse) {
-        badAnswers += 1;
-        score -= 1;
-      }
-    }
-  } */
 
   const explanationMessage = explanationScore(questionNumber, questionData, userAnswer, language);
-
-  const answerMessage = {
-    fr: ['Tout faux', 'Oups', 'Non', 'Presque', 'Parfait !'],
-    en: ['Nope', 'Oops', 'No', 'Almost there', 'Perfect!'],
-  };
 
   return (
     <section className="answerQuestion">
