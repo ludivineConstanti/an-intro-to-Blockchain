ad2ec12cb7f5304c4717ea406a3830dc1b17770c
Alexandre Richard <alexandrichard99@gmail.com>
2021-03-23 11:27:04 +0100

    Apov9 to ApoV10

--- src/middlewares/baseUrl.js
+++ src/middlewares/baseUrl.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
-export const baseUrl = 'https://apov9.herokuapp.com';
+export const baseUrl = 'https://apov10.herokuapp.com';
 export const baseUrlFront = 'localhost:8080';
